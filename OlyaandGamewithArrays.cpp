686d10c76709a05419675d6f0bbea59a61d90e38
kritagya-khanna <kritagykhanna21k@gmail.com>
2025-08-22 23:44:17 +0530

    Olya and Game with Arrays

--- OlyaandGamewithArrays.cpp
+++ OlyaandGamewithArrays.cpp
@@ -1,11 +1,36 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+long long solve(int n, vector<vector<int>> &v){
+    if(n == 1) return v[0][0];
+    sort(v.begin(), v.end(), [](vector<int>&a, vector<int>&b){return a[1] < b[1];});
+    long long ans = 0, mini = LLONG_MAX;
+    for(int i = 1; i < n; i++){
+        ans+=v[i][1];
+        mini = min(1ll*v[i][0], mini);
+    }
+    mini = min(mini, 1ll*v[0][1]);
+    mini = min(mini, 1ll*v[0][0]);
+    ans+= mini;
+    return ans;
+}
+
 int main(){
     int t;
     cin>>t;
     while(t--){
-        cout<<"hello";
+        int n;
+        cin>>n;
+        vector<vector<int>> v(n);
+        for(int i = 0; i < n; i++){
+            int m;
+            cin>>m;
+            vector<int> temp(m);
+            for(int j = 0; j < m; j++) cin>>temp[j];
+            sort(temp.begin(), temp.end());
+            v[i] = temp;
+        }
+        cout<<solve(n, v)<<endl;
     }
     return 0;
 }
